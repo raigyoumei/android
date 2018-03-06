d342158b8bd353a62b748b77b736a3405db51fcd
renxiaodong <renxiaodong@helixinsoft.com>
2018-03-06 11:21:03 +0800

    update button size

--- app/src/main/java/co/yaw/tpw/smartinspection/CarActivity.java
+++ app/src/main/java/co/yaw/tpw/smartinspection/CarActivity.java
@@ -1,18 +1,36 @@
 package co.yaw.tpw.smartinspection;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yaw.tpw.smartinspection.R;
 
-public class CarActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     final Context context = this;
     private Button nextBtn;
+    private Spinner carNumberSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +42,65 @@ public class CarActivity extends AppCompatActivity {
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context , VisualCheckActivity.class);
+                Intent intent = new Intent(context, VisualCheckActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        setSpinnerAdapter();
+    }
+
+    private void setSpinnerAdapter() {
+        carNumberSpinner = findViewById(R.id.car_number_spinner);
+        carNumberSpinner.setOnItemSelectedListener(this);
+
+        List<String> categories = new ArrayList<String>();
+        categories.add("品川47　ぬ24-869");
+        categories.add("札幌500 れ92-52");
+        categories.add("仙台78  か36-57");
+        categories.add("三河60　あ88-90");
+        categories.add("大阪20　き78-20");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        carNumberSpinner.setAdapter(dataAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), "選択: " + item, Toast.LENGTH_LONG).show();
+        popup();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    public void popup() {
+        LayoutInflater inflater = getLayoutInflater();
+        View alertLayout = inflater.inflate(R.layout.car_selector, null);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setView(alertLayout);
+        alert.setCancelable(false);
+
+        final AlertDialog dialog = alert.create();
+        dialog.show();
+
+        TextView cancleBtn = alertLayout.findViewById(R.id.cancle_button);
+        cancleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        TextView okBtn = alertLayout.findViewById(R.id.ok_button);
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
             }
         });
     }
