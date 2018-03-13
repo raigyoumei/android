fc14d2fc1e0b60858cb94778136d98fdb99cda56
renxiaodong <renxiaodong@helixinsoft.com>
2018-03-13 13:29:15 +0800

    update visiable by Looper

--- app/src/main/java/co/yaw/tpw/smartinspection/maskview/CameraMaskView.java
+++ app/src/main/java/co/yaw/tpw/smartinspection/maskview/CameraMaskView.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
@@ -34,23 +35,31 @@ public class CameraMaskView extends CameraView {
     @Override
     public void start() {
         super.start();
-        post(new Runnable() {
-            @Override
-            public void run() {
-                show();
-            }
-        });
+        if(Looper.myLooper() == Looper.getMainLooper()){
+            show();
+        } else {
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    show();
+                }
+            });
+        }
     }
 
     @Override
     public void stop() {
         super.stop();
-        post(new Runnable() {
-            @Override
-            public void run() {
-                hide();
-            }
-        });
+        if(Looper.myLooper() == Looper.getMainLooper()){
+            hide();
+        } else {
+            post(new Runnable() {
+                @Override
+                public void run() {
+                    hide();
+                }
+            });
+        }
     }
 
     private void show() {
